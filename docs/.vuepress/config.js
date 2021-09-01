4b9a53673f18955be2b4f41217f6ede8aa4e7ae5
ninthreezy <michaeldnguyen93@gmail.com>
2021-09-01 17:26:30 -0400

    fix: i think I fixed it for real

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,12 @@ const plugins = require('./plugins')
 
 module.exports = context => ({
   title: 'DataTorch Documentation',
-  head: [
-    {
+  head: [[
+    'link', {
       rel: 'stylesheet',
       href:
         'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'
-    }
+    }]
   ],
   locales: {
     '/': {
